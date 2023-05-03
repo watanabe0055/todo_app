81832a53a1652dfe109369ae9cf0b0270798a70d
watanabe0055 <mailok1212+github@gmail.com>
2023-05-03 12:42:13 +0900

    fix

--- src/parts/create/index.tsx
+++ src/parts/create/index.tsx
@@ -1,3 +1,4 @@
+import { Box, Button, Stack, TextField } from '@mui/material';
 import { useForm } from 'react-hook-form';
 
 type FormDate = {
@@ -12,25 +13,49 @@ export const TasKCreate = () => {
         formState: { errors },
     } = useForm<FormDate>();
 
-    const onSubmit = (data: any) => {
+    const onSubmit = (data: FormDate) => {
         console.log(data);
     };
 
     return (
         <>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <label htmlFor="">タイトル</label>
-                <input
-                    id="title"
-                    type="text"
-                    placeholder="title"
-                    {...register('title', { required: '※必須項目' })}
-                />
-                <p>{errors.title?.message}</p>
-                <label htmlFor="">コンテンツ</label>
-                <input id="title" type="text" placeholder="title" {...register('content')} />
-                <button type="submit">送信</button>
-            </form>
+            <Box
+                sx={{
+                    padding: 2,
+                    backgroundColor: '#C0DBEA',
+                    borderRadius: 3,
+                    '&:hover': {
+                        backgroundColor: '#C0DBEA',
+                        opacity: [0.9, 0.8, 0.7],
+                    },
+                }}
+            >
+                <Stack spacing={{ xs: 1, sm: 2 }} direction="row" useFlexGap flexWrap="wrap">
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <TextField
+                            id="title"
+                            label="タイトル"
+                            type="text"
+                            variant="outlined"
+                            {...register('title', { required: '※必須項目' })}
+                            sx={{ width: 500 }}
+                        />
+                        <p>{errors.title?.message}</p>
+                        <TextField
+                            id="content"
+                            label="コンテンツ"
+                            type="text"
+                            variant="outlined"
+                            {...register('content')}
+                            sx={{ width: 500 }}
+                        />
+                        <p>{errors.content?.message}</p>
+                        <Button variant="outlined" type="submit">
+                            送信
+                        </Button>
+                    </form>
+                </Stack>
+            </Box>
         </>
     );
 };
