2ca8cbaba3324099bb0fe1eb4f293b1bad6f3fbb
watanabe0055 <mailok1212+github@gmail.com>
2023-05-04 23:44:14 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,26 @@ import { useState } from 'react';
 import './App.css';
 import { FormDate, TasKCreate } from './parts/create';
 import { TaskIndex } from './parts/index';
+import { Alert, AlertTitle } from '@mui/material';
 
 function App() {
     const [todoList, setTodoList] = useState<FormDate[]>([]);
-    const [title, setTitle] = useState<string>();
-    const [content, setContent] = useState<string>();
     const handleFormSubmit = (data: FormDate) => {
         setTodoList((prevTodoList) => [...prevTodoList, data]);
     };
     return (
         <>
             <TasKCreate onFormSubmit={handleFormSubmit} />
-            {todoList.map((todo, index) => (
-                <TaskIndex title={todo.title} content={todo.content} key={index} />
-            ))}
+            {todoList.length ? (
+                todoList.map((todo, index) => (
+                    <TaskIndex title={todo.title} content={todo.content} key={index} />
+                ))
+            ) : (
+                <Alert severity="info">
+                    <AlertTitle>未登録</AlertTitle>
+                    <strong>TODOタスク</strong>が登録されていません
+                </Alert>
+            )}
         </>
     );
 }
--- src/parts/index/index.tsx
+++ src/parts/index/index.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import { Button, ButtonGroup, Grid } from '@mui/material';
 import { FormDate } from '../create';
 
 export const TaskIndex = (data: FormDate) => {
     const { title, content } = data;
     return (
         <>
-            <p>{title}</p>
-            <p>{content}</p>
+            <Grid
+                container
+                alignItems="center"
+                justifyContent="center"
+                sx={{ marginTop: 1, padding: 3, backgroundColor: '#FCC8D1', borderRadius: 2 }}
+            >
+                <Grid item xs={10}>
+                    <h3>タイトル:{title}</h3>
+                    <h3>コンテンツ:{content}</h3>
+                </Grid>
+                <Grid item xs={2}>
+                    <ButtonGroup
+                        variant="text"
+                        size="large"
+                        color="secondary"
+                        aria-label="text button group"
+                    >
+                        <Button>完了</Button>
+                        <Button>削除</Button>
+                    </ButtonGroup>
+                </Grid>
+            </Grid>
         </>
     );
 };
